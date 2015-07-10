ab7af011593dd3a61abf7a64a6e21b6535d2a625
unknown <hgupta@Training2.internal.tavisca.com>
2015-07-10 12:32:14 +0530

    operator overloading implemented

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Money amount1 = null;
+            Money amount2 = null;
+            Money money3 = null;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter Amount 1 (Amount Currency): ");
+                    amount1 = new Money(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception Occured.");
+                    Console.WriteLine(e.Message);
+                    
+                    continue;
+                }
+                break;
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter Amount 2 (Amount Currency): ");
+                     amount2 = new Money(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception Occured.");
+                    Console.WriteLine(e.Message);
+                    
+                    continue;
+                }
+                break;
+
+            }
+            try
+            {
+                money3 = amount1 + amount2;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                Console.ReadKey();
+                return;
+            }
+            Console.Write("The Total Amount is: ");
+            Console.Write(money3.Amount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Money.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using OperatorOverloading.Model;
+using Operator_Overloading.Model;
+
+namespace OperatorOverloading.Model
+{
+    public class Money
+    {
+        private double _amount;
+        private string _currency;
+
+        public double Amount
+        {
+            get { return _amount; }
+            private set
+            {
+                if (double.IsInfinity(value))
+                {
+                    throw new OverflowException();
+                }
+                _amount = value;
+            }
+        }
+
+        public string Currency
+        {
+            get { return _currency; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new InvalidCurrencyException(Messages.InvalidCurrency);
+                }
+                _currency = value;
+            }
+        }
+
+        public Money(double amount, string currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public Money(string inputAmount)
+        {
+
+            if (string.IsNullOrWhiteSpace(inputAmount))
+            {
+                throw new System.Exception(Messages.NullExcception);
+            }
+
+            var amountArr = inputAmount.Split(' ');
+            double amount;
+
+            if (amountArr.Length != 2)
+            {
+                throw new System.Exception();
+            }
+
+            if (double.TryParse(amountArr[0], out amount))
+            {
+                Amount = amount;
+            }
+            else
+            {
+                throw new System.Exception(Messages.InvalidAmount);
+            }
+
+            Currency = amountArr[1];
+        }
+
+        public static Money operator +(Money obj1, Money obj2)
+        {
+            if (obj1 == null || obj2 == null)
+            {
+                throw new Exception(Messages.NullExcception);
+            }
+            if (obj1.Currency.Equals(obj2.Currency, StringComparison.OrdinalIgnoreCase))
+            {
+                double totalAmount = obj1.Amount + obj2.Amount;
+                return new Money(totalAmount, obj1.Currency);
+            }
+            else
+            {
+                throw new System.Exception(Messages.CurrencyMismatch);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Amount + " " + Currency;
+        }
+    }
+}
